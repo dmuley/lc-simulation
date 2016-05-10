14f03ec969c78ba537783469f7e02417a1a5cd3f
Dhruv Muley <dhruv.muley@gmail.com>
2016-05-10 09:45:16 -0700

    Breaking off general transit processing into its own file.

--- transit_models/kepler16.py
+++ transit_models/kepler16.py
@@ -13,9 +13,7 @@ if __name__ == '__main__':
 import numpy as np;
 from itertools import chain;
 import matplotlib.pyplot as plt;
-
-#STEPS = 1001;
-#REVOLUTIONS = 0.125;
+import general_transits as gt;
 
 #OPERATING EXAMPLE TO RUN THIS CODE
 #FOR RETROGRADE ORBITS: Add pi radians to inclination, add pi radians to arg_periastron
@@ -114,77 +112,17 @@ plt.ylabel("Difference from mean (AU)");
 plt.xlabel("Time (days)");
 plt.title("Sky-projected deviation of bodies in planetary system from COM");
 plt.xlim(times[0], times[len(times) - 1]);
-
-
 plt.show();		
 
+fx, fy, fz, zpos = gt.sort_keys(final_x, final_y, final_z);
+lb, intersects = gt.arrange_combinations(fx, fy, transit_array);
 
-###### LOOM ALGORITHM ######
-# Using the Z-values we will index the orders of X and Y values, and then get transits for each body.
-
-fx = np.array(final_x).T;
-fy = np.array(final_y).T;
-fz = np.array(final_z).T;
-
-zpos = np.argsort(fz);
-
-print len(fx), len(times);
-
-### measuring intersections
-
-ta_elements = np.arange(0,len(transit_array));
-ta_combos = itertools.combinations(list(ta_elements), 2);
-intersects = (fx * 0).T;
-
-print len(ta_elements), len(intersects);
-
-for a in ta_combos:
-	if (transit_array[a[0]].temperature != 0 or transit_array[a[1]].temperature != 0):
-       		intersects[a[0]][np.where((fx.T[a[0]] - fx.T[a[1]])**2 + (fy.T[a[0]] - fy.T[a[1]])**2 < (transit_array[a[0]].radius + transit_array[a[1]].radius)**2)] += 1;
-       		intersects[a[1]][np.where((fx.T[a[0]] - fx.T[a[1]])**2 + (fy.T[a[0]] - fy.T[a[1]])**2 < (transit_array[a[0]].radius + transit_array[a[1]].radius)**2)] += 1;
-
-#	print np.where((fx.T[a[0]] - fx.T[a[1]])**2 + (fy.T[a[0]] - fy.T[a[1]])**2 < (transit_array[a[0]].radius + transit_array[a[1]].radius)**2);
-
-
-light_blocked = np.zeros(len(fx));
-
-#### ACTUAL TRANSIT MODELING BELOW ####
-
-#Number of transit-modeling integration steps per timestep
 n = 31;
 ta = transit_array;
-
-for m in range(0,len(light_blocked)):
-	#Excluding bodies that do not intersect at this frame
-	#Need >0.1 because some bodies without any intersections are noted to have -0 intersections (off by some epsilon).
-	c = np.array([[ta[r].radius * 1000., fx[m][r] * 1000., fy[m][r] * 1000.] for r in zpos[m]])[intersects.T[m][zpos[m]] > 0.1];
-	lum = np.array([ta[s].temperature for s in zpos[m]])[intersects.T[m][zpos[m]] > 0.1];
-	
-	t = 0.;
-	
-	#Excluding as stars bodies with zero luminosity
-	for i in np.where(lum != 0.)[0]:
-		ct = c[i:len(c)];
-		ct.T[1] -= ct[0][1];
-		ct.T[2] -= ct[0][2];
-
-		ti = getTangentsIntersections(ct);
-
-		if not ((ti[1] % np.pi) < np.zeros(len(ti[1])) + 0.01).all():
-			rt = generateRadiiThetas(n, ti[0], ti[1], ti[2]);
-			f = rd2(rt, ct, opt = 0);
-			oh = groupAndIntegrate(bounds = f, num = n, star_rad = ct[0][0], ld_coeff = [1.9, 0., 1., 0.], ld_power = [0.0, 0.5, 1., 1.5]);
-			
-			del rt, f;
-			oh *= lum[i]
-			t += oh;
-				
-	print times[m], t
-	
-	light_blocked[m] = t;
+cadence, light_blocked = gt.generate_lightcurve(fx, fy, lb, intersects, n, ta, times, zpos);
 
 #Plotting final light curve.
 plt.clf();
-plt.plot(times, -light_blocked, '-');
-plt.xlim(times[0], times[len(times) - 1]);
+plt.plot(cadence, -light_blocked, '-');
+plt.xlim(cadence[0], cadence[len(times) - 1]);
 plt.show();
